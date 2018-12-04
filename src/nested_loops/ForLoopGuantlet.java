eb448a1f26dfb7080ac6fc3305d66e6379516eb4
nicholaskimsd <league@WTS-IM-17.attlocal.net>
2018-12-03 16:58:32 -0800

    module 5

--- src/nested_loops/ForLoopGuantlet.java
+++ src/nested_loops/ForLoopGuantlet.java
@@ -5,8 +5,46 @@ public class ForLoopGuantlet {
 		for (int i = 0; i < 101; i++) {
 			System.out.println(i);
 			}
+		for (int o = 100; o < 0; o--) {
+			System.out.println(o);
+		}
+		for (int v = 2; v < 101; v++) {
+			if (v%2==0) {
+				System.out.println(v);
+			}
+		}
+		for (int p = 1; p < 99; p++) {
+			if (p%2!=0) {
+				System.out.println(p);
+			}
+		}
+		for (int t = 1; t < 501; t++) {
+			if (t%2==1) {
+				System.out.println(t+ " is even");
+			}
+			else if (t%2!=1) {
+				System.out.println(t+ " is odd");
+			}
+		}
+		for (int k = 0; k < 778; k++) {
+			if (k%7==0) {
+				System.out.println(k);
+			}
+		}
+			
+			for (int m = 2008; m < 2019; m++) {
+				System.out.println("In "+m+" I was "+(m-2008)+ " years old.");
+			}
+			for (int q = 0; q < 3; q++) {
+				String lot = ""+q;
+				for (int j = 0; j < 3; j++) {
+					System.out.println(lot+" "+j);
+				}
+				
+			}
+		}
 		
 		
 	}
 
-}
+
